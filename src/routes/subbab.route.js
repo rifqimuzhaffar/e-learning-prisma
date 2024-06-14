0cb71a7ba02b64d1b077b16b04ea1a2bf6bdb289
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-06-14 13:32:30 +0700

    verifyyoken

--- src/routes/subbab.route.js
+++ src/routes/subbab.route.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const route = express.Router();
 
+const { verifyToken } = require("../middlewares/auth");
 const { index } = require("../controllers/subbab.controller");
 
-route.get("/", index);
+route.get("/", verifyToken, index);
 
 module.exports = route;
